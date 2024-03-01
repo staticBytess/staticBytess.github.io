9b2145394a1e50268d29adab1e71d3cbc3b16478
staticBytess <160567397+staticBytess@users.noreply.github.com>
2024-03-01 14:56:44 -0800

    Tester page was added. Removed tests from home page

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -13,6 +13,20 @@ class myHeader extends HTMLElement {
     }
 }
 
+class dropDownMenu extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+                <a href="projects.html" onmouseenter="myFunction()" class="dropbtn">Projects</a>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="piCar.html">Raspberry Pi "Car"</a>
+                    <a href="website.html">Portfolio Website</a>
+                    <a href="#">Reaction Time Game</a>
+                </div>
+        `
+    }
+}
+
+
 class myFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -27,6 +41,9 @@ class myFooter extends HTMLElement {
 
 customElements.define('my-header', myHeader)
 customElements.define('my-footer', myFooter)
-customElements.define('start-pack', starterPack)
+customElements.define('drop-down', dropDownMenu)
+
+
+
 
 
