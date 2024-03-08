a7a016ad4acd249e1a10f949af308e46e8342dc8
staticBytess <160567397+staticBytess@users.noreply.github.com>
2024-03-07 21:46:14 -0800

    Removed external script file for scroll

--- scripts/instruct-scroll.js
+++
@@ -1,6 +0,0 @@
-const element = document.querySelector("div#instruct-down");
-const output = document.querySelector("p#output");
-
-element.addEventListener("scroll", (event) => {
-    element.opacity = 0;
-})
